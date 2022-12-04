1c09a0fb5177d71b55e2e2221d6a3f6f76252f7f
Guilherme Gonçalves <guilhermepgx20@gmail.com>
2022-12-03 22:04:19 -0300

    Cadastro e senha
    Criando tela de cadastro e login.

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Faça seu login</title>
+</head>
+<body>
+    <h1>Acesse sua conta</h1>
+    <form action="" methot="POST">
+        <p>
+            <label for="">E-mail</label>
+            <input type="text" name="email">
+        </p>
+
+        <p>
+            <label for="">Senha</label>
+            <input type="password" name="senha">
+        </p>
+        <p>
+            <button type="submit">Entrar</button>
+        </p>
+
+    </form>
+</body>
+</html>
